1cd74e7cd4e38fc4435b36adc5e7242c07f8880c
h <g>
2021-11-16 12:06:43 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 15, b = 30;
+            double i, z;
+            for (i = a; i <= b; i++)
+            {
+                z = 1.8 * i + 32;
+                Console.WriteLine($"C={i}   f={z}");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
